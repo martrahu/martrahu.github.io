72a861680662939b8e58318ca80098738e1879ac
martrahu <122733239+martrahu@users.noreply.github.com>
2023-10-06 20:52:32 +0300

    Update scripts.js

--- websivu/assets/javascript/scripts.js
+++ websivu/assets/javascript/scripts.js
@@ -77,7 +77,7 @@ function temp() {
         .then(response => response.json())
         .then(data => {
             document.getElementById("kuva").src = "/websivu/assets/pictures/" + ticker.substring(0, ticker.length - 3).toLowerCase() + ".png"
-            document.getElementById("themebtn").value = "/websivu/assets/pictures/"+ticker.toLowerCase().substring(0, ticker.length - 3).toString() + ".png"
+            //document.getElementById("themebtn").value = "/websivu/assets/pictures/"+ticker.toLowerCase().substring(0, ticker.length - 3).toString() + ".png"
             const close = new Array();
 
             const pvm = new Array();
