5c2d7910d54a16c2be8bca8b27a029f03e5fd197
martrahu <martrahu@hotmail.com>
2023-10-10 20:28:27 +0300

    uusi

---
+++ websivu/README.md
@@ -0,0 +1,1 @@
+html taulukoille en keksinyt käyttöä, muuten pitäisi olla ok
--- websivu/assets/javascript/scripts2.js
+++ websivu/assets/javascript/scripts2.js
@@ -6,11 +6,11 @@ function myFunction_set() {
     var rs = String(getComputedStyle(r).getPropertyValue('--blue'));
 
     if (rs === "#0c1483") {
-        document.getElementById("themebtn").value = "Light theme"
+        document.getElementById("themebtn").value = "light theme"
         r.style.setProperty('--blue', '#e2effb');
         r.style.setProperty('--lightblue', '#0c1483');
     } else if (rs.valueOf == "#e2effb".valueOf) {
-        document.getElementById("themebtn").value = "Dark theme"
+        document.getElementById("themebtn").value = "dark theme"
         r.style.setProperty('--blue', '#0c1483');
         r.style.setProperty('--lightblue', '#e2effb');
     }
